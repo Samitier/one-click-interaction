8faef85f508ed1f35f12cd1b15d0023baa8f3de4
Blai Samitier <blai@samitier.me>
2019-11-21 01:08:11 +0100

    create project

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,108 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="main" :class="darkClass">
+  <div class="container" @click="setWhite">
+    <img src="./assets/img/white_bkg.png" />
+    <img class="ship" src="./assets/img/white_ship.png" />
+    <img class="dark-img" src="./assets/img/dark_bkg.png" />
+    <img class="dark-img ship" src="./assets/img/dark_ship.png" />
   </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      isWhite: false
+    };
+  },
+  computed: {
+    darkClass() {
+      return this.isWhite ? "" : "dark"
+    }
+  },
+  methods: {
+    setWhite() {
+      this.isWhite = true;
+    }
   }
-}
+};
 </script>
 
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+html, body {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.main {
+  display: flex;
+  align-items: center;
+  background #e6dbce;
+
+  &.dark {
+    background: #050505;
+
+    .container {
+      border-color #ddd;
+    }
+    .dark-img {
+      opacity 1
+    }
+  }
+}
+
+.container {
+  width: 600px;
+  height: 350px;
+  overflow: hidden;
+  margin: auto;
+  position: relative;
+  cursor pointer
+  border 3px solid black
+}
+
+.main, img, .container {
+  transition: all 4s;
+}
+
+.main, img {
+  position: absolute;
+  width: 100%;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+}
+
+.dark-img {
+  opacity 0
+}
+
+.ship {
+  animation: shipAnim 4s alternate infinite;
+}
+
+@keyframes shipAnim {
+  0% {
+    transform: rotateZ(0.7deg);
+  }
+
+  40% {
+    transform: translateY(3px) rotateZ(0);
+  }
+
+  100% {
+    transform: rotateZ(-0.7deg);
+  }
+}
+
+@media screen and (max-width: 610px) {
+  .container {
+    width 350px
+    height: 205px
+  }
+}
 </style>
